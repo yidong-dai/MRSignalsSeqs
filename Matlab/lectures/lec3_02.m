0359321eafd0987e9d3ecbdbd07daa7e91be6e04
cnh78890 <Pepedison233b!>
2022-08-28 22:05:57 +0200

    made some extra comments to "lec3_02.m"

--- Matlab/lectures/lec3_02.m
+++ Matlab/lectures/lec3_02.m
@@ -7,18 +7,18 @@ M = [0;0;1];	% Equilibrium
 Ne = 10;		% Frames - excitation
 Np = 50;		% Frames - precession
 Nrot = 1;		% cycles
-Ry = yrot(-90/Ne);	% Rotation about My
-Rz = zrot(360/Nrot/Np);	% Rotation about Mz
+Ry = yrot(-90/Ne);	% Rotation about My, excitation, animation for 90 degree
+Rz = zrot(360/Nrot/Np);	% Rotation about Mz, precession, animation for 360 degree
 % Add: [A,B] = relax(...)	
 
 figure(3); disp('Excitation/Rotation');
-plotm(M);
-for n=1:Ne; 
+plotm(M); % firstly plot equilibrium
+for n=1:Ne; % Plot Ne frames of excitation with flip angle equals 90 degree
   M=Ry*M; plotm(M);	% Rotate and plot
   drawnow;
 end;
 
-for n=1:Np; 
+for n=1:Np;  % Plot Np frames of precession with 360 degree
   M=Rz*M; plotm(M);	% Rotate and plot
   drawnow;
 end;
